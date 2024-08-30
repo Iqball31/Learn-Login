95be48953431498ea0602ed2127211557da002a4
Iqball31 <Iqbalahmad31900@gmail.com>
2024-08-30 20:21:48 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,10 +1,13 @@
 <?php
-// Dummy signup logic for demonstration
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     // Here you would normally save the user to a database
     echo 'User registered successfully!';
+} else {
+    // If the request method is not POST, return a 405 error
+    header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+    echo '405 Method Not Allowed';
 }
 ?>
